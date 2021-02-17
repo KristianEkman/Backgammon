bf247bf6f6259264bca5403bc7dfd426527e33c2
Kristian Ekman <kristian.ekman.swe@gmail.com>
2021-02-17 07:45:02 +0100

    deploy

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  socketServiceUrl: 'wss://localhost:44394/ws',
-  apiServiceUrl: 'https://localhost:44394'
+  socketServiceUrl: 'wss://backgammon.azurewebsites.net/ws',
+  apiServiceUrl: 'https://backgammon.azurewebsites.net/'
 };
