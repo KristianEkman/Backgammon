9621b648e0846ecdf543303f1f8fb46db37d3653
Kristian Ekman <kristian.ekman.swe@gmail.com>
2021-05-15 09:08:06 +0200

    Mailer simple test.

---
+++ Backend/Mail/Mailer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace Backend.Mail
+{
+    public class Mailer
+    {
+        public static void SendTest()
+        {
+            // trusting my own self sign certificate on my smtp server.
+            ServicePointManager.ServerCertificateValidationCallback =
+            delegate (
+                object s,
+                X509Certificate certificate,
+                X509Chain chain,
+                SslPolicyErrors sslPolicyErrors
+            ) {
+                return true;
+            };
+
+            var client = new SmtpClient("smtp1.kristianekman.com", 2525);
+            string to = "kristian.ekman.swe@gmail.com";
+            string from = "kristian@kristianekman.com";
+            string subject = "Testar SMTP client.";
+            string body = @"Test Tjurgränd 47.";
+            MailMessage message = new MailMessage(from, to, subject, body);
+            client.Credentials = new NetworkCredential("kristian", "***");
+            client.EnableSsl = true;
+
+            client.Send(message);
+        }
+    }
+}
---
+++ BackendTests/MailTests.cs
@@ -0,0 +1,12 @@
+﻿using Backend.Mail;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class MailTest
+{
+    [TestMethod]
+    public void TestMail()
+    {
+        Mailer.SendTest();
+    }
+}
